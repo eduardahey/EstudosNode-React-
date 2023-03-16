b7e3b4a9fc97da2d990db175848e257830c3b4ed
eduarda <cavalcantemariads@gmail.com>
2023-03-16 10:26:30 -0300

    versao2

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
-export default function Header(props){
+// export default function Header(props){
+//     return (
+//         <header>
+//             <h1>{props.children}</h1>
+//         </header>
+//     );
+// }
+
+// import React from 'react';
+
+// export default function Header(props){
+//     return (
+//         <header>
+//             <h1>{props.title}</h1>
+//         </header>
+//     );
+// }
+
+export default function Header({children}){
     return (
         <header>
-            <h1>{props.title}</h1>
+            <h1>{children}</h1>
         </header>
     );
 }
-
 // export default Header;
